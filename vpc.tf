b9496a6f7da1a8e360d50640f16e529300427f4d
Nazeeya <nazeeya.shikalgar@gmail.com>
2021-11-05 13:17:00 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,12 @@
+module "network" {
+source  = "app.terraform.io/dbnaz/network/google"
+version = "3.4.0"
+network_name = "nazeeya-network"
+project_id = var.project
+subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
